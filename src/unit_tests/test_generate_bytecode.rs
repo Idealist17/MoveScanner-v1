641cd9e05b6488bf5d0cfbc9d454af8fdc26f16a
Liteng-Cpp <3110074895@qq.com>
2023-06-14 15:20:00 +0800

    address 16 => 32

--- src/unit_tests/test_generate_bytecode.rs
+++ src/unit_tests/test_generate_bytecode.rs
@@ -8,7 +8,7 @@ use crate::move_ir::bytecode_display;
 
 #[test]
 fn test_generate_bytecode() {
-    let filename = PathBuf::from_str("/Users/lteng/Movebit/detect/build/movebit/bytecode_modules/witness.mv").unwrap();
+    let filename = PathBuf::from_str("/Users/lteng/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/b9d331b4a8e35aa95826433692d9aa9c/Message/MessageTemp/4e7aa6f1fa7f648d128ea5ad15fb0247/File/router.mv").unwrap();
     let cm = compile_module(filename);
     for fd in &cm.function_defs { 
         let mut bg = StacklessBytecodeGenerator::new(&cm, fd);
